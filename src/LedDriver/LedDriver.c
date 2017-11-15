82e9dd43d0445d598172ef41a34d42fdd5050b38
Your Name <Your Email>
2017-11-15 12:10:26 +0900

    Ref CheckOutOfbounds Method

--- src/LedDriver/LedDriver.c
+++ src/LedDriver/LedDriver.c
@@ -1,8 +1,10 @@
 #include "LedDriver.h"
 #include "RuntimeError.h"
+#include <stdbool.h>
 
 static uint16_t convertLedNumberToBit(int ledNumber);
 static void updateHardware(void);
+static bool IsLedOutOfBounds(int ledNumber);
 
 enum {ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON};
 
@@ -18,7 +20,7 @@ void LedDriver_Create(uint16_t * address)
 
 void LedDriver_TurnOn(int ledNumber)
 {
-    if (ledNumber <= 0 || ledNumber > 16)
+    if (IsLedOutOfBounds(ledNumber))
     {
         RUNTIME_ERROR("LED Driver: out-of-bounds LED", -1);
         return;
@@ -29,7 +31,7 @@ void LedDriver_TurnOn(int ledNumber)
 
 void LedDriver_TurnOff(int ledNumber)
 {
-    if (ledNumber <= 0 || ledNumber > 16)
+    if (IsLedOutOfBounds(ledNumber))
         return;
     ledsImage &= ~(convertLedNumberToBit(ledNumber));
     updateHardware();
@@ -49,6 +51,12 @@ static uint16_t convertLedNumberToBit(int ledNumber)
 static void updateHardware(void)
 {
     *ledsAddress = ledsImage;
+}
+enum {FIRST_LED = 1, LAST_LED = 16};
+
+static bool IsLedOutOfBounds(int ledNumber)
+{
+    return (ledNumber < FIRST_LED || ledNumber > LAST_LED);
 }
 
 void LedDriver_Destroy(void)
