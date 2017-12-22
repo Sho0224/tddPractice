179099fe79ce4d0b925e734edd8f25be1c84b2c9
Sho <daxjps@gmail.com>
2017-12-22 11:31:14 +0900

    Add LightScheduler

---
+++ include/HomeAutomation/LightScheduler.h
@@ -0,0 +1,10 @@
+#ifndef LIGHT_SCEDULER_H_
+#define LIGHT_SCEDULER_H_
+
+#include "TimeService.h"
+
+void LightScheduler_Create(void);
+void LightScheduler_Destroy(void);
+void LightScheduler_Wakeup(void);
+void LightScheduler_ScheduleTurnOn(int id, Day day, int minuteOfDay);
+#endif // LIGHT_SCEDULER_H_
---
+++ src/HomeAutomation/LightScheduler.c
@@ -0,0 +1,21 @@
+#include "LightScheduler.h"
+
+void LightScheduler_Create(void)
+{
+
+}
+
+void LightScheduler_Destroy(void)
+{
+
+}
+
+void LightScheduler_Wakeup(void)
+{
+    
+}
+
+void LightScheduler_ScheduleTurnOn(int id, Day day, int minuteOfDay)
+{
+
+}
